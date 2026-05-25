d7694372bd8f314be85701134039e275ad8b00a6
Aditya Patel <4118adi@gmail.com>
2026-05-25 22:02:35 +0530

    Rework

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-
-
-
-import Image from "next/image";
-
-
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-100 to-purple-200 dark:from-gray-900 dark:to-gray-800 p-6 transition-colors">			
