75a12149ba40fb81c70b58b365d33f60a4aa4009
Aditya Patel <4118adi@gmail.com>
2026-05-25 22:01:17 +0530

    Restart

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,11 @@ export const metadata = {
 };
 
 
-import ThemeToggle from "../components/ThemeToggle";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 		return (
 			<html lang="en" suppressHydrationWarning>
 				<body className="bg-gradient-to-br from-blue-100 to-purple-200 dark:from-gray-900 dark:to-gray-800 transition-colors min-h-screen">
-					<ThemeToggle />
 					{children}
 				</body>
 			</html>
