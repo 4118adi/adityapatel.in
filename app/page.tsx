8504532f96aa07cbca285d08726997eb1a5a0a62
Aditya Patel <4118adi@gmail.com>
2026-05-26 10:07:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -95,17 +95,19 @@ export default async function Home() {
 					<p className="motion-item text-sm">{resume.intro}</p>
 					<p className="motion-item pt-2 text-sm">{resume.summary}</p>
 					{resume.skills.length > 0 ? (
-						<p className="motion-item pt-2 text-sm">
-  <span className="inline-flex flex-wrap items-center gap-2">
-    I primarily use
+						<div className="motion-item pt-2 text-sm text-justify">
+  <p>
+    I primarily use the following technologies for building my software.
+  </p>
+
+  <div className="mt-2 flex flex-wrap gap-2">
     {resume.skills.map((skill, index) => (
       <Chip index={index} key={skill}>
         {skill}
       </Chip>
     ))}
-    for building my software.
-  </span>
-</p>
+  </div>
+</div>
 					) : null}
 				</div>
 			</header>
